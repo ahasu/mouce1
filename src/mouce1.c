67bc87fc6dd804edd8d47544592e0cf25c4a6aea
ahasu <ahasu96@gmail.com>
2017-10-25 15:47:39 +0900

    Eclipse_firtst_time

--- src/mouce1.c
+++ src/mouce1.c
@@ -1,7 +1,7 @@
 #include	"iodefine.h"
 #include	"def.h"
 #include	"init.h"
-//#include	"util7125.h"
+#include	"serial.h"
 volatile int cnt;
 
 //50MHz 0.0000002s 5000で1ms
@@ -16,6 +16,7 @@ void delay(int time){
 }
 
 void main(void){
+	GREEN=1;
 	init();
 	initCMT();
 	initAD();
@@ -30,18 +31,24 @@ void main(void){
 
 
 	while(1){
-		GREEN = 0;		//ピンinit
-		LED_CL = 0;
-		LED_CR = 0;
-		LED_L = 0;
-		LED_R = 0;		//ここまで
+//		GREEN = 0;		//ピンinit
+		LED_CL = 1;
+		LED_CR = 1;
+		LED_L = 1;
+		LED_R = 1;		//ここまで
 		ADC();
 
+		myprintf("Light Time %d , %d , %d , %d\n\r",SEN_L,SEN_CL,SEN_CR,SEN_R);
+
+/*
 		if(SEN_CL<500);//LED_CL = 1;
+
 		if(SEN_CR<500);//LED_CR = 1;
+
 		if(SEN_L<500)//LED_L = 1;
 			GREEN = 1;
-		if(SEN_R<500);//LED_R = 1;
 
+		if(SEN_R<500);//LED_R = 1;
+		*/
 	}
 }
